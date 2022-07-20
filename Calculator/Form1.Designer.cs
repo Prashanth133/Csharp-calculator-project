107006be0310a068e7f0a64f3420718a339faa48
Prashanth133 <71934965+Prashanth133@users.noreply.github.com>
2022-07-20 17:44:29 +0530

    files uploaded

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Font = new System.Drawing.Font("Verdana", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNum1.Location = new System.Drawing.Point(271, 71);
+            this.txtNum1.MaxLength = 4;
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(150, 46);
+            this.txtNum1.TabIndex = 0;
+            this.txtNum1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Font = new System.Drawing.Font("Verdana", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNum2.Location = new System.Drawing.Point(271, 165);
+            this.txtNum2.MaxLength = 4;
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(150, 46);
+            this.txtNum2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 34);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "First number:\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 34);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Second number:\r\n";
+            // 
+            // btnMul
+            // 
+            this.btnMul.Font = new System.Drawing.Font("Verdana", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMul.Location = new System.Drawing.Point(486, 172);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(112, 50);
+            this.btnMul.TabIndex = 5;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Font = new System.Drawing.Font("Verdana", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDiv.Location = new System.Drawing.Point(634, 172);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(112, 50);
+            this.btnDiv.TabIndex = 7;
+            this.btnDiv.Text = " ÷";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Font = new System.Drawing.Font("Verdana", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSub.Location = new System.Drawing.Point(634, 100);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(112, 50);
+            this.btnSub.TabIndex = 6;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblResult.ForeColor = System.Drawing.Color.Navy;
+            this.lblResult.Location = new System.Drawing.Point(152, 294);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(150, 45);
+            this.lblResult.TabIndex = 8;
+            this.lblResult.Text = "<result>";
+            this.lblResult.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(26, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 34);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Result:\r\n";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Verdana", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.Location = new System.Drawing.Point(486, 100);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(112, 50);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Name = "Form1";
+            this.Text = "MyCalculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        string num1str, num2str;
+        float num1, num2;
+        float result = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblResult.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            getNumbers();
+            result = num1 + num2;
+
+            lblResult.Text = result.ToString();
+
+        }
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            getNumbers();
+            result = num1 - num2;
+
+            lblResult.Text = result.ToString();
+
+        }
+
+
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            getNumbers();
+            result = num1 * num2;
+
+            lblResult.Text = result.ToString();
+
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            getNumbers();
+            result = (float) num1 / num2;
+
+            lblResult.Text = result.ToString();
+
+        }
+
+        private void  getNumbers()
+        {
+            try
+            {
+                num1str = this.txtNum1.Text.ToString();
+                num2str = this.txtNum2.Text.ToString();
+
+                num1 = float.Parse(num1str);
+                num2 = float.Parse(num2str);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Please enter numbers.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
